37a53238c989757add93046a6abe0394de2a4c26
Masuk <masukrahoman5@gmail.com>
2026-03-19 17:15:59 +0600

    Implement Update and Query all notes

--- FastApi_With_Mongodb/app/main.py
+++ FastApi_With_Mongodb/app/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI,HttpException,status
+from fastapi import FastAPI,HTTPException,status
 from fastapi.responses import JSONResponse
 from bson import ObjectId
 from datetime import datetime
@@ -41,3 +41,50 @@ async def create_note(note:NoteCreate):
 
     created_note = await collection.find_one({"_id":result.inserted_id})
     return helper_note(created_note)
+
+
+@app.get("/notes",response_model=list[NoteResponse])
+async def get_all_notes():
+    db = get_database()
+    collection = db.notes
+
+    notes = await collection.find({}).sort("created_at",-1).to_list(length=100)
+    return [helper_note(note) for note in notes]
+
+
+@app.put("/update/{note_id}",response_model=NoteResponse)
+async def update_note(note_id:str,note:NoteUpdate):
+
+    if not ObjectId.is_valid(note_id):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid ID Formate"
+        )
+    
+    db = get_database()
+    collection = db.notes
+
+    update_data = note.model_dump(exclude_unset=True)
+
+    if not update_data:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "No data has to be updated"
+        )
+    
+    result = await collection.update_one(
+        {"_id":ObjectId(note_id)},
+        {"$set":update_data}
+    )
+
+    if result.matched_count == 0:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Note not found"
+        )
+    
+    updated_note = await collection.find_one({"_id":ObjectId(note_id)})
+
+    return helper_note(updated_note)
+
+
